0e1c759a5ca65675c0964007df1dd4730140a2b8
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-07 12:05:33 -0300

    Exercício 02 da parte 2 do dia 04 do bloco 04 de javaScript.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte_2_funcoes.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte_2_funcoes.js
@@ -12,5 +12,20 @@ function verificaPalindrome (word) {
 
 console.log(verificaPalindrome('tenet'));
 
+// Exercício 02 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+function maiorValor (array) {
+  let maior = 0;
+
+  for (let index in array) {
+    if (array[index] > array[maior] ) {
+      maior = index;
+    }
+  }
+  return maior;
+}
+
+console.log(maiorValor([2, 3, 6, 7, 10, 1]));
+
 
 
