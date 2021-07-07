e22b513262155faa59de466ff18fe83b46e1fb75
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-07 11:34:11 -0300

    Exercício 04 da parte 1 do dia 04 do bloco 04 de javaScript.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte_1_objetos_e_for_in.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte_1_objetos_e_for_in.js
@@ -26,3 +26,19 @@ for (let index in info) {
   console.log(info[index]);
 }
 
+//Exercício 05 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+
+let infoTwo = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+
+for (let index in info) {
+  if (index === 'recorrente' && info[index] === 'Sim' && infoTwo[index] === 'Sim') {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[index] + ' e ' + infoTwo[index]);
+  }
+}
