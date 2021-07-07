1e984c46df31f1cee3a4df3e290bedfed679e1b4
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-07 13:09:03 -0300

    Exercício 07 da parte 2 do dia 04 do bloco 04 de javaScript.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte_2_funcoes.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte_2_funcoes.js
@@ -1,6 +1,6 @@
 //Exercício 01 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
 
-function verificaPalindrome (word) {
+function verificaPalindrome(word) {
   for (let index in word) {
     if (word[index] != word[(word.length - 1) - index]) {
       return false;
@@ -14,7 +14,7 @@ console.log(verificaPalindrome('tenet'));
 
 // Exercício 02 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 
-function maiorValor (array) {
+function maiorValor(array) {
   let maior = 0;
 
   for (let index in array) {
@@ -29,7 +29,7 @@ console.log(maiorValor([2, 3, 6, 7, 10, 1]));
 
 //Exercício 03 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 
-function menorValor (array) {
+function menorValor(array) {
   let menor = 0;
 
   for (let index in array) {
@@ -44,7 +44,7 @@ console.log(menorValor([2, 4, 6, 7, 10, 0, -3]));
 
 //Exercício 04 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 
-function maiorNome (nomes) {
+function maiorNome(nomes) {
   let maiorNome = nomes[0];
 
   for (let index in nomes) {
@@ -59,7 +59,7 @@ console.log(maiorNome(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']
 
 //Exercício 05 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 
-function maisRepete (numeros) {
+function maisRepete(numeros) {
   let contadorRepetidos = 0;
   let contadorNumero = 0;
   let indexRepetido = 0;
@@ -83,7 +83,7 @@ console.log(maisRepete([5, 3, 7, 20, 11, 20, 12, 20, 14, 20, 15, 17, 15]));
 
 //Exercício 06 - Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N.
 
-function somatorioDeN (n) {
+function somatorioDeN(n) {
   let somatorio = 0;
   
   for (let index = 1; index <= n; index += 1) {
@@ -94,9 +94,21 @@ function somatorioDeN (n) {
 
 console.log(somatorioDeN(5));
 
+//Exercício 07 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
 
+function verificaFimPalavra(palavra, fimPalavra) {
+  palavra = palavra.split('');
+  fimPalavra = fimPalavra.split('');
+  controle = true;
 
+  for (let index = 0; index < fimPalavra.length; index += 1) {
+    if (palavra[palavra.length - fimPalavra.length + index] != fimPalavra[index]) {
+      controle = false;
+    }
+  }
+  return controle;
+}
 
-
+console.log(verificaFimPalavra('matheus', 'us'));
 
 
