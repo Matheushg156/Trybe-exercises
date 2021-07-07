d747c9c6bcd5a44026f2a20a6f4e32675ffa2fda
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-07 11:10:07 -0300

    Exercício 02 da parte 1 do dia 04 do bloco 04 de javaScript.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte_1_objetos_e_for_in.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte_1_objetos_e_for_in.js
@@ -4,4 +4,15 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
+//Exercício 01 - Imprimir mensagem de boas-vinda.
+
 console.log('Bem-vinda ' + info.personagem);
+
+//Exercício 02 - Adicionar (recorrente: sim) no objeto "info".
+
+info.recorrente = 'Sim';
+
+console.log(info);
+
+
+
