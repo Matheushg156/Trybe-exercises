62ddd338dd9850f0ee9eaeff401225fd8a4f4b59
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-12 21:26:49 -0300

    Exercício 04.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,4 +1,4 @@
-//4 - Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+
 //5 - Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
 //6 - Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
 //7 - Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
@@ -23,6 +23,8 @@ const tagSection = document.createElement('section');
 tagSection.className = 'center-content';
 tagMain.appendChild(tagSection);
 
+//4 - Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
 
-
-
+const paragraph = document.createElement('p');
+paragraph.innerHTML = 'Muitos projetos para fazer e pouco tempo';
+tagSection.appendChild(paragraph);
