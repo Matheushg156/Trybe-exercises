25a125263047ac13650184caf5f66b2ddf8aae51
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-12 22:29:56 -0300

    Segunda parte - exercício 03.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -74,7 +74,11 @@ for (let index = 0; index <3; index += 1) {
   classH3[index].className = 'description';
 }
 
-//3 - Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
+//3 - Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild();
+
+const removeLeft = document.getElementsByClassName('left-content')[0];
+tagMain.removeChild(removeLeft);
+
 //4 - Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section ;
 //5 - Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
 //6 - Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
