bb36735630fd78c4b05346b9ed681850f59d5701
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-14 19:35:40 -0300

    Finalizando os exemplos do dia 03 do bloco 05 de javascript.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
@@ -4,21 +4,10 @@ const thirdLi = document.getElementById('thirdLi');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
-/*
- 1 - Copie esse arquivo e edite apenas ele;
-
-
-
-5 - Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-a cor do mesmo;
-
-Segue abaixo um exemplo do uso de event.target:
-*/
-
 //2 - Crie uma função que adicione a classe 'tech' ao elemento selecionado;
 //2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
-function handleChangeTech (event) {
+function handleChangeTech(event) {
   const techElement = document.querySelector('.tech');
   techElement.classList.remove('.tech');
   event.target.classList.add('tech');
@@ -33,7 +22,7 @@ thirdLi.addEventListener ('click', handleChangeTech);
 
 //3 - Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento com a classe 'tech';
 
-function changeTextTech (event) {
+function changeTextTech(event) {
   const techElement = document.querySelector('.tech');
   techElement.innerHTML = event.target.value;
 }
@@ -43,8 +32,22 @@ input.addEventListener('input', changeTextTech);
 //4 - Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele redirecione para alguma página; 
 //4.1. Que tal redirecionar para seu portifólio?
 
-function redirecionar () {
+function redirecionar() {
   window.location.replace('https://blog.betrybe.com/');
 }
 
 myWebpage.addEventListener('dblclick', redirecionar);
+
+//5 - Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere a cor do mesmo;
+
+function changeColor(event) {
+  event.target.style.color = 'blue';
+}
+
+myWebpage.addEventListener('mouseover', changeColor);
+
+function changeColorBack(event) {
+  event.target.style.color = 'unset';
+} 
+
+myWebpage.addEventListener('mouseout', changeColorBack);
