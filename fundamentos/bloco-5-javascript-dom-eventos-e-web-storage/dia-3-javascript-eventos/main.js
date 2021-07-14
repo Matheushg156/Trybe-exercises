acbc92839a2dd171a1250d342d91340d8f535539
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-14 19:25:23 -0300

    Exemplo 04 do dia 03 do bloco 05 de javascript.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
@@ -8,9 +8,7 @@ const myWebpage = document.getElementById('mySpotrybefy');
  1 - Copie esse arquivo e edite apenas ele;
 
 
-4 - Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-redirecione para alguma página;
-4.1. Que tal redirecionar para seu portifólio?
+
 5 - Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 a cor do mesmo;
 
@@ -35,7 +33,18 @@ thirdLi.addEventListener ('click', handleChangeTech);
 
 //3 - Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento com a classe 'tech';
 
-input.addEventListener('input', function(event)) {
+function changeTextTech (event) {
   const techElement = document.querySelector('.tech');
   techElement.innerHTML = event.target.value;
 }
+
+input.addEventListener('input', changeTextTech);
+
+//4 - Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele redirecione para alguma página; 
+//4.1. Que tal redirecionar para seu portifólio?
+
+function redirecionar () {
+  window.location.replace('https://blog.betrybe.com/');
+}
+
+myWebpage.addEventListener('dblclick', redirecionar);
