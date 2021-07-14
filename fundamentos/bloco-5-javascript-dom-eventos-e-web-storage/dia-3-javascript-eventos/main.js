0c00a23da07a18d37309a004b9066facb71f4927
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-14 19:16:29 -0300

    Exemplo 03 do dia 03 do bloco 05 de javascript.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
@@ -7,8 +7,7 @@ const myWebpage = document.getElementById('mySpotrybefy');
 /*
  1 - Copie esse arquivo e edite apenas ele;
 
-3 - Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech';
+
 4 - Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 redirecione para alguma página;
 4.1. Que tal redirecionar para seu portifólio?
@@ -33,3 +32,10 @@ firstLi.addEventListener('click', handleChangeTech);
 secondLi.addEventListener ('click', handleChangeTech);
 
 thirdLi.addEventListener ('click', handleChangeTech);
+
+//3 - Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento com a classe 'tech';
+
+input.addEventListener('input', function(event)) {
+  const techElement = document.querySelector('.tech');
+  techElement.innerHTML = event.target.value;
+}
