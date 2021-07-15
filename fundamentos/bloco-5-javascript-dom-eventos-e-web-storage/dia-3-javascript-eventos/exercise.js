9325bd966597855ba12ddbd106269ea66d8e6840
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-14 23:49:48 -0300

    Exercício 06 do dia 03 do bloco 05 de javascript.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercise.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercise.js
@@ -113,3 +113,25 @@ function modifyTextButton() {
 
 modifyTextButton();
 
+// Exercício 06
+
+function dayMouseOver() {
+  let days = document.querySelector('#days');
+
+  days.addEventListener('mouseover', function(event) {
+    event.target.style.fontSize = '30px';
+    event.target.style.fontWeight = '600';
+  })
+};
+
+function dayMouseOut() {
+  let days = document.querySelector('#days');
+
+  days.addEventListener('mouseout', function(event) {
+    event.target.style.fontWeight = '200';
+    event.target.style.fontSize = '20px';
+  })
+};
+
+dayMouseOver();
+dayMouseOut();
