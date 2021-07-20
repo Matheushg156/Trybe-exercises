87003c6285db86e6cb6fc3f650c0308d7b79dc80
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-20 00:46:51 -0300

    Exercício finalizado.

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -1,20 +1,34 @@
 function createStateOptions() {
-  let states = document.getElementById('state');
-  const stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
-
+  const states = document.getElementById('state');
+  const stateOptions = ['Selecione seu estado', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
   for (let index = 0; index < stateOptions.length; index += 1) {
-    let createOptions = document.createElement('option');
+    const createOptions = document.createElement('option');
     states.appendChild(createOptions).innerText = stateOptions[index];
     states.appendChild(createOptions).value = stateOptions[index];
   }
 }
 
-createStateOptions();
+const picker = new Pikaday({
+  field: document.getElementById('datepicker'),
+  format: 'D/M/YYYY',
+  toString(date, format) {
+    // you should do formatting based on the passed format,
+    // but we will just return 'D/M/YYYY' for simplicity
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+    // dateString is the result of `toString` method
+    const parts = dateString.split('/');
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+    return new Date(year, month, day);
+  }
+});
 
-const submitButton = document.querySelector('.submit-button');
-
-function handleSubmit(event) {
-  event.preventDefault();
+window.onload = function () {
+  createStateOptions();
 }
-
-submitButton.addEventListener('click', handleSubmit);
