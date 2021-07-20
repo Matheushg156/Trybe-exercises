c493279ed8e8bfb733184dec1b567363ed67c65b
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-19 23:03:27 -0300

    Exercícios do dia 02 do bloco 06 de HTML e CSS.

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,20 @@
+function createStateOptions() {
+  let states = document.getElementById('state');
+  const stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+
+  for (let index = 0; index < stateOptions.length; index += 1) {
+    let createOptions = document.createElement('option');
+    states.appendChild(createOptions).innerText = stateOptions[index];
+    states.appendChild(createOptions).value = stateOptions[index];
+  }
+}
+
+createStateOptions();
+
+const submitButton = document.querySelector('.submit-button');
+
+function handleSubmit(event) {
+  event.preventDefault();
+}
+
+submitButton.addEventListener('click', handleSubmit);
