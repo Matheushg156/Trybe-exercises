99e537a4de7a3bde3ad84689d60ca6326d6d24c0
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-09-03 00:27:50 -0300

    Exercise fixation 3.

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import './App.css';
 
-function handleClick() {
-    console.log('Clicou no botão!');
-}
-
-function handleClick2() {
-  console.log('Xablau!');
-}
-
-function handleClick3() {
-  console.log('Xulambs!');
-}
-
 class App extends React.Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+    this.handleClick3 = this.handleClick3.bind(this)
+  }
+
+  handleClick() {
+    console.log('Clicou no botão!');
+  }
+
+    handleClick2() {
+      console.log('Xablau!');
+  }
+
+    handleClick3() {
+      console.log('Xulambs!');
+  }
+
   render() {
     return (
       <div>
-        <button onClick={ handleClick }>Botão 1</button>
-        <button onClick={ handleClick2 }>Botão 2</button>
-        <button onClick={ handleClick3 }>Botão 3</button>
+        <button onClick={ this.handleClick }>Botão 1</button>
+        <button onClick={ this.handleClick2 }>Botão 2</button>
+        <button onClick={ this.handleClick3 }>Botão 3</button>
       </div>
     );
   }
