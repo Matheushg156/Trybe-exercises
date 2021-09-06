f4a9808515e846ab8f1a85694a8dbc9621a5d430
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-09-06 15:15:45 -0300

    Exercise fixation 01.

---
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/form-exercise-fixation/src/components/Form.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      name: '',
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      name: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
+        <form>
+          <label>
+            Nome:
+            <input
+              id="name"
+              name="name"
+              type="text"
+              onChange={this.handleChange}
+              value={ this.state.name }
+            />
+          </label>
+
+          <label>
+            Email:
+            <input
+              id="email"
+              name="email"
+              type="email"
+            />
+          </label>
+
+          <label>
+            Idade:
+            <select
+              id="age"
+              name="age"
+              defaultValue=""
+            >
+              <option value="">Selecione</option>
+              <option value="adult">Maior que 18</option>
+              <option value="underage">Menor que 18</option>
+            </select>
+          </label>
+
+          <label>
+            Fale sobre você:
+            <textarea
+              id="aboutYou"
+              nme="aboutYou"
+            />
+          </label>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Form;
+
+
+
+
+
+
+  
